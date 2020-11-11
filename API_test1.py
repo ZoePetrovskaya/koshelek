118e2ea960438f41fa85e3e77aa082af8a7d2afd
Zoe Petrovskaya <zoya0820@mail.ru>
2020-11-11 19:53:42 +0300

    sandbox-api.coinmarketcap

--- API_test1.py
+++ API_test1.py
@@ -2,11 +2,11 @@ print("#Тестирование API")
 
 
 from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
-import os
-#url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' #песочница
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+from datetime import datetime 
+
+url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' #песочница
+
 parameters = {
   'start':'1',
   'limit':'10',
@@ -15,8 +15,8 @@ parameters = {
 }
 headers = {
   'Accepts': 'application/json',
-  #'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c', #ключ для песочницы
-  'X-CMC_PRO_API_KEY': '05acc93f-3277-4d90-854d-7661d306874f',
+  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c', #ключ для песочницы
+ 
 }
 
 session = Session()
@@ -25,25 +25,35 @@ session.headers.update(headers)
 try:
   response = session.get(url, params=parameters)
   json_data = json.loads(response.text)
-#  print(json_data)
+  print("Запрос на url:",response.url)
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
 
 print("Код ответа:", response.status_code)
-assert response.status_code == 200, response.status_code
+assert response.status_code == 200
+
 print("Время ответа :", response.elapsed.total_seconds()*1000, "мс")
-assert response.elapsed.total_seconds()*1000 <= 1000, "Время ответа превышает допустимое"
+assert response.elapsed.total_seconds()*1000 <= 500, response.elapsed.total_seconds()*1000
+
+print("Размер пакета данных:", len(response.content),"байт")
+assert len(response.content) < 10*1024
 
 
 for each in json_data['data']:
     print("Наименование валюты:", each['name'])
-    print("дата последнего обновления:", each['last_updated'])
+    print("дата последнего обновления:", each['last_updated'].split("T")[0])
 
-file_data = open('file_response.text', 'w')
-file_data.write(str(json_data))
+
+
+today =  datetime.strftime(datetime.now(), "%Y-%m-%d")
+print("Текущая дата:",today)
+#assert today == each['last_updated'].split("T")[0], each['last_updated'].split("T")[0]
+
+print("данные по запросу записаны в файл:file_response.text ")
+file_data = open('file_response.text', 'wb')
+file_data.write(response.content)
 file_data.close()
 
 
-print("Размер файла:", os.stat('file_response.text').st_size/1024 ,"Кб")
-assert os.stat('file_response.text').st_size/1024 < 10, "Размер данных превышает допустимой значение"
 print("Успешно")
